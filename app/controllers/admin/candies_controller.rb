eb8a460cb1b53f9cf38f21af38e7ff96f6c2e8df
Chad Ellison <chad.ellison0123@gmail.com>
2016-03-04 22:42:23 -0700

    Add edit functionality to admin
    admin can can access all admin_candies_path through admin dashboard
    admin can edit from there or click on individual candy to go to show
    could easily implement edit button on show page as well--design decision
    link and buttons still need styling

--- app/controllers/admin/candies_controller.rb
+++ app/controllers/admin/candies_controller.rb
@@ -1,2 +1,25 @@
 class Admin::CandiesController < Admin::AuthorizationController
+  def index
+    @candies = Candy.all
+  end
+
+  def show
+    @candy = Candy.find(params[:id])
+  end
+
+  def edit
+    @candy = Candy.find(params[:id])
+  end
+
+  def update
+    candy = Candy.find(params[:id])
+    candy.update(candy_params)
+    redirect_to admin_candies_path
+  end
+
+  private
+
+  def candy_params
+    params.require(:candy).permit(:title, :description, :price, :image, :status)
+  end
 end
--- spec/features/admin_can_edit_candies_spec.rb
+++ spec/features/admin_can_edit_candies_spec.rb
@@ -2,10 +2,8 @@ require "rails_helper"
 
 RSpec.feature "admin can edit candies" do
   scenario "admin sees updated candies" do
-
     candy1 = create(:candy)
-
-    admin = User.create(username: "jones", name: "jones", password: "password", role: 1)
+    User.create(username: "jones", name: "jones", password: "password", role: 1)
 
     visit root_path
     click_on "Login"
@@ -14,26 +12,23 @@ RSpec.feature "admin can edit candies" do
     click_on "Sign in"
 
     click_on "View All Candies"
+
     expect(current_path).to eq admin_candies_path
-
-    within("img") do
-      expect(page).to have_content "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSHDOSNxmatCFt8kQLujghKqW3dlmgvBhMrqBd747RP95UQTuDzRF7838s"
-    end
-
+    expect(page).to have_css("img[src*='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSHDOSNxmatCFt8kQLujghKqW3dlmgvBhMrqBd747RP95UQTuDzRF7838s']")
     expect(page).to have_content candy1.description
-    expect(page).to have_content candy1.price
+    expect(page).to have_content candy1.to_dollars
     expect(page).to have_content candy1.status
 
     click_on candy1.title
-    expect(current_path).to eq admin_candy_path
+    expect(current_path).to eq admin_candy_path(candy1.id)
 
-    visit admin_dashboard_path
+    click_on "Back to Candies"
 
     click_on "Edit"
 
     fill_in "Title", with: "chocolate bar"
     fill_in "Description", with: "It is juicy"
-    fill_in "Price", with: "10"
+    fill_in "Price", with: "1000"
     fill_in "Status", with: "Out of stock"
     fill_in "Image", with: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7PwjqnGRs8WnjryyEr8MZuI1YK7X_qfPLy6tMrdxnEHtmivVbzQ"
     click_on "Update Candy"
@@ -42,8 +37,6 @@ RSpec.feature "admin can edit candies" do
     expect(page).to have_content "It is juicy"
     expect(page).to have_content "$10.00"
     expect(page).to have_content "Out of stock"
-    within("img") do
-      expect(page).to have_content "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7PwjqnGRs8WnjryyEr8MZuI1YK7X_qfPLy6tMrdxnEHtmivVbzQ"
-    end
+    expect(page).to have_css "img[src*='https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7PwjqnGRs8WnjryyEr8MZuI1YK7X_qfPLy6tMrdxnEHtmivVbzQ']"
   end
 end
