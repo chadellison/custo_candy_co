e30db9e14d5c2f9c9656ea599e530c4fde7bbf87
Nathan Venn <natevenn@gmail.com>
2016-03-06 14:21:54 -0700

    fix syntax error

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe Order, type: :model do
   describe Order do
-  context "can have many candies" do
-    it { should have_many(:candy_orders) }
-    it { should have_many(:candies) }
-    it { should belong_to(:user) }
-  end
+    context "can have many candies" do
+      it { should have_many(:candy_orders) }
+      it { should have_many(:candies) }
+      it { should belong_to(:user) }
+    end
 
-  it "it formats the date" do
-    user = User.create(username: "jones", name: "jones", password: "password")
-    order = Order.create(status: "pending", user_id: user.id)
-    order.update(created_at: "2016-03-06 20:52:29")
-    expect("Sunday March 6 2016").to eq order.format_date
+    it "it formats the date" do
+      user = User.create(username: "jones", name: "jones", password: "password")
+      order = Order.create(status: "pending", user_id: user.id)
+      order.update(created_at: "2016-03-06 20:52:29")
+      expect("Sunday March 6 2016").to eq order.format_date
+    end
   end
 end
