43dea662a30c219526f73ad3a467e2eba63caa52
Chad Ellison <chad.ellison0123@gmail.com>
2016-03-07 14:00:20 -0700

    Add passage of candy_id instead of format

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,15 +1,15 @@
 class RatingsController < ApplicationController
   def new
-    @candy = Candy.find(params[:format])
+    @candy = Candy.find(params[:candy_id])
     @rating = @candy.ratings.new
   end
 
   def create
-    Rating.create(rating: params[:rating][:rating], candy_id: params[:format])
+    Rating.create(rating_params)
     redirect_to candies_path
   end
 
   def rating_params
-    params.require(:rating).permit(:rating, :format)
+    params.require(:rating).permit(:rating, :candy_id)
   end
 end
