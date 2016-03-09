3759bed5619bbe619535242314a73bf6d0f5e8f4
Chad Ellison <chad.ellison0123@gmail.com>
2016-03-08 17:44:36 -0700

    Add candy show page list of past reviews
    add candy show page displays more info, including category, status, and rating
    add more info link that links to candy show page
    add link on candy show page to add a review
    candy show page still needs styling

--- spec/models/candy_spec.rb
+++ spec/models/candy_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Candy, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:category_id) }
     it { should validate_presence_of(:status) }
+    it { should belong_to(:category) }
   end
   context "price format" do
     it "turns cents into dollars" do
