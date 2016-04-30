3bf5ec8f58a018a09f994e278e27e343dd576f57
Chad Ellison <chad.ellison0123@gmail.com>
2016-04-30 02:43:58 -0600

    Add carts and cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,32 +8,33 @@ Rails.application.routes.draw do
   get "/admin/candies/:id/edit",    to: "admin/candies#edit"
   patch "/admin/candies/:id",       to: "admin/candies#update"
 
-  get "/admin/dashboard/:id", to: "admin/dashboard#show"
+  get "/admin/dashboard/:id",       to: "admin/dashboard#show"
 
-  get "/candies", to: "candies#index"
-  get "/candies/:id", to: "candies#show"
+  get "/candies",                   to: "candies#index"
+  get "/candies/:id",               to: "candies#show"
 
-  get "/categories", to: "categories#index"
-  get "/categories/:id", to: "categories#show"
+  get "/categories",                to: "categories#index"
+  get "/categories/:id",            to: "categories#show"
 
-  get "/reviews", to: "reviews#new"
-  post "/reviews", to: "reviews#create"
+  get "/reviews",                   to: "reviews#new"
+  post "/reviews",                  to: "reviews#create"
 
-  get "/register", to: "users#new"
+  get "/register",                  to: "users#new"
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get "/login",                     to: "sessions#new"
+  post "/login",                    to: "sessions#create"
+  delete "/logout",                 to: "sessions#destroy"
 
-  post "/users", to: "users#create"
-  get "/dashboard", to: "users#show"
+  post "/users",                    to: "users#create"
+  get "/dashboard",                 to: "users#show"
 
-  resources :carts, only: [:create]
-  get "/cart", to: "carts#show"
-  put "/cart", to: "carts#update"
-  delete "/cart", to: "carts#destroy"
+  post "/carts",                    to: "carts#create"
+  get "/cart",                      to: "carts#show"
+  put "/cart",                      to: "carts#update"
+  delete "/cart",                   to: "carts#destroy"
 
   resources :orders, only: [:index, :show, :create]
+
   get "/order_success", to: "order_success#show"
 
   resources :custom_candies, only: [:new, :create, :show, :index, :update]
